9594d309345148113754e34ccab4615364cecbb4
Savas Vedova <savas@vsk.me>
2019-04-27 14:56:31 +0200

    Enable server side rendering

---
+++ create-react-app/src/server.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { renderToString } from "react-dom/server";
+import App from "./App.js";
+
+export default (req, res) => {
+  // We don't need bots indexing this page
+  if (req.path === "/robots.txt") {
+    return res.send({
+      body: "User-agent: *\nDisallow: /",
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8"
+      }
+    });
+  }
+
+  return res.send({
+    headers: {
+      "X-Powered-By": "stormkit.io",
+      "X-My-Custom-Header": Date.now()
+    },
+    body: { content: renderToString(<App />) },
+    status: 200
+  });
+};
