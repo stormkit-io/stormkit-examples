3832bbcc095928fba3be2ca164be18fbaf504c47
Savas Vedova <savas@stormkit.io>
2023-11-29 14:58:06 +0300

    chore: update nuxt project

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,39 +1,4 @@
-import { defineNuxtConfig } from 'nuxt3'
-import ViteComponents from 'unplugin-vue-components/vite'
-import IconsResolver from 'unplugin-icons/resolver'
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  meta: {
-    title: 'A Better Nuxt 3 Starter',
-  },
-  buildModules: [
-    'nuxt-windicss',
-    '@pinia/nuxt',
-    'unplugin-icons/nuxt',
-    '@vueuse/nuxt',
-    [
-      '@intlify/nuxt3',
-      {
-        localeDir: 'locales',
-        vueI18n: {
-          locale: 'en'
-        }
-      }
-    ]
-  ],
-  windicss: {
-    analyze: true,
-  },
-  vite: {
-    plugins: [
-      ViteComponents({
-        resolvers: [
-          IconsResolver({
-            componentPrefix: '',
-          }),
-        ],
-        dts: true,
-      }),
-    ],
-  },
+  devtools: { enabled: true }
 })
