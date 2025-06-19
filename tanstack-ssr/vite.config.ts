861e61a664f0bfaceb11fb71b0b62f470d578ef8
Savas Vedova <savas@stormkit.io>
2025-06-19 10:40:41 +0300

    chore: enable prerendering

--- tanstack-ssr/vite.config.ts
+++ tanstack-ssr/vite.config.ts
@@ -1,6 +1,6 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
@@ -8,8 +8,15 @@ export default defineConfig({
   },
   plugins: [
     tsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
-    tanstackStart(),
+    tanstackStart({
+      spa: {
+        prerender: {
+          enabled: true,
+          crawlLinks: true,
+        },
+      },
+    }),
   ],
-})
+});
